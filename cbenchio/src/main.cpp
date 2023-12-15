67ce1fde5eb74ff0400f193a9ed85cc808f6c654
lparisi <l.parisi@epcc.ed.ac.uk>
2023-12-15 18:10:05 +0000

    Added yaml input interface

--- cbenchio/src/main.cpp
+++ cbenchio/src/main.cpp
@@ -17,10 +17,32 @@ int main(int argc, char ** argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nRanks);
 
-    //YAML::Node config = YAML::LoadFile("config.yaml");
+    YAML::Node config = YAML::LoadFile("config.yaml");
 
+    auto api = config["benchmark"]["API"].as<std::string>();
+
+    auto comm=MPI_COMM_WORLD;
+    auto shape = config["benchmark"]["shape"].as<std::array<int,3> >();
+    std::string basename = "data";
+
+        
+    if (api == "posix")
+    {
+        auto filePerProcess = config["benchmark"]["filePerProcess"].as<bool>();
+
+        if (filePerProcess)
+        {
+            comm=MPI_COMM_SELF;
+            shape[0]/=nRanks;
+            basename="data" + std::to_string(rank);
+        }   
+        
+
+    }
     
-    distributedCartesianArray data(MPI_COMM_WORLD, { 1000000000, 1, 1  });
+
+
+    distributedCartesianArray data(comm, shape);
 
     indexDataGenerator gen;
     if (rank==0)
@@ -30,8 +52,8 @@ int main(int argc, char ** argv)
     
     gen.generate(data); 
 
-  
-    auto writer = posix_io("data" + std::to_string(rank) );
+    
+    auto writer = posix_io( basename );
 
     benchmark current_benchmark("posix");
 
