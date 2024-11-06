c561541f4bcbb5a9e853409753b1bbade67b3842
lucaparisi91 <luca.parisi91@gamil.com>
2024-11-06 17:20:52 +0000

    Added install and verbose build command

--- scripts/build_archer2_cbenchio.sh
+++ scripts/build_archer2_cbenchio.sh
@@ -5,13 +5,11 @@ module load PrgEnv-gnu
 module load cray-hdf5-parallel
 source settings_archer2.sh
 
-
-
 SOURCE_DIR=$(pwd)/../cbenchio
 
 rm -rf build
 mkdir build 
 cd build 
 CXX=CC CC=cc FC=ftn cmake $SOURCE_DIR -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX/cbenchio/0.1
-make -j 4
+VERBOSE=1 make -j 4
 make install
