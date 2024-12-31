a8fbe8311d443047d5576e3d7979541127785c61
lucaparisi91 <luca.parisi91@gamil.com>
2024-12-31 13:03:17 +0000

    Allow linking with lustre

--- cbenchio/tools.cmake
+++ cbenchio/tools.cmake
@@ -4,6 +4,7 @@ function ( configure_target target )
 target_link_libraries( ${target} PUBLIC MPI::MPI_CXX )
 target_link_libraries( ${target} PUBLIC Eigen3::Eigen )
 target_link_libraries( ${target} PUBLIC yaml-cpp::yaml-cpp )
+target_link_libraries( ${target} PUBLIC lustreapi )
 
 set_property ( TARGET ${target} PROPERTY CXX_STANDARD 17 )
 
